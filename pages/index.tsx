0c9aba0f54ca4cfbb0ef23a6779f75dc123c6f9e
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2021-08-04 21:23:15 +0200

    Remove styles leftovers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Layout, siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import { Date } from "../components/date";
@@ -19,22 +18,22 @@ const Home: FC<{
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className="text-xl my-2">
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section className="text-lg my-2 p-1">
+        <h2 className="text-xl">Blog</h2>
+        <ul className="list-none">
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li className="mb-5" key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small className="text-gray-600">
                 <Date dateString={date} />
               </small>
             </li>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,7 +2,6 @@ import { Layout } from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import { Date } from "../../components/date";
-import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
 import { FC } from "react";
 
@@ -19,8 +18,8 @@ const Post: FC<{
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <h1 className="text-4xl font-extrabold">{postData.title}</h1>
+        <div className="text-gray-600 mb-6">
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
